e084094858117aece71c83a97df6f07f699394e5
Diogo <drd.rclrd@gmail.com>
2019-11-24 20:06:29 -0300

    Add new route for placeswq

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" component={Users} />
+      <Switch>
+        <Route exact path="/" component={Users} />
+        <Route exact path="/places/new" component={NewPlace} />
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 }
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Users = () => {
   return (
-    <div>Users works</div>
+    <div>Users works!</div>
   )
 }
 
