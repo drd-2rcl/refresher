7acf85b713f644808416e47d3a61a26ce026eeac
Diogo <drd.rclrd@gmail.com>
2019-12-09 21:59:32 -0300

    Add data to users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return (
-    <div>Users works!</div>
-  )
+  const USERS = [
+    {
+      id: 'u1', 
+      name: 'Diogo Dias', 
+      image: 'https://assets.imgix.net/hp/snowshoe.jpg?auto=compress&w=900&h=600&fit=crop&crop=focalpoint&fp-x=.41&fp-y=.43&fp-z=1.59&fp-debug=1',
+      places: 3
+    }
+  ];
+  return <UsersList items={USERS} />
 }
 
 export default Users;
